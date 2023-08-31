6cb7f8cad84a40a8f286deeb5f0f49b1a5d85960
ferhat_mousavi <ferhat.mousavi@gmail.com>
2023-08-31 14:43:28 +0300

    Minor change.

--- user_interface/control/numpad/numpad.py
+++ user_interface/control/numpad/numpad.py
@@ -12,7 +12,7 @@ class NumPad(QAbstractButton):
         self.numpad_height = height
         self.location_x = location_x
         self.location_y = location_y
-        print(self.numpad_width, self.numpad_height, self.location_x, self.location_y)
+        print("NumPad", self.numpad_width, self.numpad_height, self.location_x, self.location_y)
         self.setParent(parent)
 
     staticMetaObject = QtCore.QMetaObject()
